8ab620ff7747b558c4cc238a579c063f267a3aa5
ChosenTechies <89414990+urmoit@users.noreply.github.com>
2025-08-31 14:55:07 +0300

    Delete src directory

--- src/main/java/com/chosen/lib/Config.java
+++
@@ -1,7 +0,0 @@
-package com.chosen.lib;
-
-public class Config {
-    public boolean exampleFeatureEnabled = true;
-    public String welcomeMessage = "Welcome to ChosenLib!";
-    // Add more config fields as needed
-}
